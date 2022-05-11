ac431046b8f839c72ef0f925412073c137740403
yetabalij <yetabalij@gmail.com>
2022-05-11 17:07:07 +0300

    db connection modifed.

--- api/index.js
+++ api/index.js
@@ -6,16 +6,26 @@ dotenv.config();
 
 const app = express();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE);
+    console.log("database connected");
+  } catch (error) {
+    throw error;
+  }
+};
+
 app.listen(8000, () => {
+  connect();
   console.log("app started");
 });
 
-//db
-mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("database connected");
-  });
+// //db
+// mongoose
+//   .connect(process.env.DATABASE, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("database connected");
+//   });
