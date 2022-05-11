4dd477834cf5fde36f303bc1537156f40d2f564b
yetabalij <yetabalij@gmail.com>
2022-05-11 17:02:52 +0300

    mongodb installation and configuration.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
 
 const app = express();
 
 app.listen(8000, () => {
   console.log("app started");
 });
+
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("database connected");
+  });
