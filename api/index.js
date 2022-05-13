dfef96dd78f5710b9a94ebfbd73a048d6c760a63
yetabalij <yetabalij@gmail.com>
2022-05-13 11:08:10 +0300

    place holder for routing is implemented.

--- api/index.js
+++ api/index.js
@@ -2,9 +2,21 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+//imported routeres
+const authRoute = require("../api/routes/auth");
+const authHotels = require("../api/routes/hotels");
+const authRooms = require("../api/routes/rooms");
+const authUsers = require("../api/routes/users");
+
 dotenv.config();
 
 const app = express();
+
+//router middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", authHotels);
+app.use("/api/rooms", authRooms);
+app.use("/api/users", authUsers);
 
 const connect = async () => {
   try {
