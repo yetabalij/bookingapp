5def7a33379d1b989a361f58f14061654b1edeed
yetabalij <ermiastsegu@yahoo.com>
2022-05-14 17:09:45 +0300

    auth is applied.

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -6,15 +6,16 @@ const {
   getHotel,
   getAllHotel,
 } = require("./../controlers/hotel");
+const { verifyToken, verifyAdmin } = require("./../utils/verityToken");
 
 const router = express.Router();
 
 //CREATE
-router.post("/", createHotel);
+router.post("/", verifyToken, verifyAdmin, createHotel);
 //UPDATE
-router.put("/:id", updateHotel);
+router.put("/:id", verifyToken, verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyToken, verifyAdmin, deleteHotel);
 //GET
 router.get("/:id", getHotel);
 //GET ALL
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,16 +5,21 @@ const {
   getUser,
   getAllUsers,
 } = require("./../controlers/user");
+const {
+  verifyToken,
+  verifyUser,
+  verifyAdmin,
+} = require("./../utils/verityToken");
 
 const router = express.Router();
 
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyToken, verifyUser, updateUser);
 //DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyToken, verifyUser, deleteUser);
 //GET
-router.get("/:id", getUser);
+router.get("/:id", verifyToken, verifyUser, getUser);
 //GET ALL
-router.get("/", getAllUsers);
+router.get("/", verifyToken, verifyAdmin, getAllUsers);
 
 module.exports = router;
