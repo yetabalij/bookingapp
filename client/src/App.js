2a6bda810095b542d2d256f9cac72c941fa31a55
yetabalij <ermiastsegu@yahoo.com>
2022-05-15 16:25:25 +0300

    home page component is created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Home></Home>
     </div>
   );
 }
