6ee7cb533d993ae838f49b90d644a17a465a2f5f
yetabalij <ermiastsegu@yahoo.com>
2022-05-15 17:30:28 +0300

    basic routing is implemented.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Flights from "./pages/Flights";
+import CarRentals from "./pages/CarRentals";
+import Attractions from "./pages/Attractions";
+import AirportTaxi from "./pages/AirportTaxi";
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/flight" element={<Flights />} />
+        <Route path="/carrentals" element={<CarRentals />} />
+        <Route path="/attractions" element={<Attractions />} />
+        <Route path="/airporttaxi" element={<AirportTaxi />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
