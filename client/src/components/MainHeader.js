48fb07284e90c09c7b8ddbc9d7d28c202f7939cd
yetabalij <yetabalij@gmail.com>
2022-05-16 12:00:54 +0300

    color MainHeader.js is completed.

--- client/src/components/MainHeader.js
+++ client/src/components/MainHeader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { useNavigate } from "react-router-dom";
 
 const MainHeaderContainer = styled.div`
   ${tw`
@@ -9,10 +10,64 @@ const MainHeaderContainer = styled.div`
   height:35.5vh
 `;
 
+const ContentContainer = styled.div`
+  ${tw`
+    w-3/5
+    ml-auto
+    mr-auto
+    pt-12
+    justify-items-center
+    `}
+`;
+const Title = styled.h1`
+  ${tw`
+        text-white
+        text-5xl
+        font-semibold
+    `}
+`;
+const ParagraphContainer = styled.div`
+  ${tw`
+        w-4/5
+    `}
+`;
+const Paragraph = styled.p`
+  ${tw`
+        text-white
+        text-2xl
+        pt-4
+        tracking-normal
+    `}
+`;
+const SigninRegisterButton = styled.button`
+  ${tw`
+        bg-secondary-color
+        mt-10
+        px-5
+        py-3
+        text-white
+        font-semibold
+    `}
+`;
+
 const MainHeader = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <MainHeaderContainer>MainHeader</MainHeaderContainer>
+      <MainHeaderContainer>
+        <ContentContainer>
+          <Title>A Lifetime of discounts? It's Genius.</Title>
+          <ParagraphContainer>
+            <Paragraph>
+              Get rewarded for your travels – unlock instant savings of 10% or
+              more with a free Booking.com account
+            </Paragraph>
+          </ParagraphContainer>
+          <SigninRegisterButton onClick={() => navigate("/signin")}>
+            Signin / Register
+          </SigninRegisterButton>
+        </ContentContainer>
+      </MainHeaderContainer>
     </div>
   );
 };
