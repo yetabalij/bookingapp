b685e7c94135c3f32d7723cd095ee0b788763533
yetabalij <yetabalij@gmail.com>
2022-05-16 16:13:27 +0300

    location picker completed.

--- client/src/pages/HomePage/components/HotelPicker.js
+++ client/src/pages/HomePage/components/HotelPicker.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBed } from "@fortawesome/free-solid-svg-icons";
 
 const HotelPickerContainer = styled.div`
   ${tw`
@@ -9,7 +11,6 @@ const HotelPickerContainer = styled.div`
     ml-auto
     mr-auto
     mt--6
-    p-4
     justify-items-center
     bg-white
     `}
@@ -19,14 +20,50 @@ const HotelPickerContainer = styled.div`
 `;
 const InputContainer = styled.div`
   ${tw`
-
+        flex
     `}
+`;
+const LocationInputContainer = styled.div`
+  ${tw`
+    
+    `}
+  border-style: solid;
+  border-width: 2px;
+  border-color: #febb02;
+`;
+const LocationInput = styled.select`
+  ${tw`
+    w-96
+    `}
+  outline: none;
+  padding: 15px;
 `;
 const HotelPicker = () => {
   return (
     <div>
       <HotelPickerContainer>
-        <InputContainer>HotelPicker</InputContainer>
+        <InputContainer>
+          <LocationInputContainer>
+            <FontAwesomeIcon
+              icon={faBed}
+              style={{ color: "gray", marginLeft: "20px" }}
+            />
+            <LocationInput>
+              <option
+                style={{ color: "gray" }}
+                value=""
+                selected
+                disabled
+                hidden
+              >
+                Where are you going?
+              </option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </LocationInput>
+          </LocationInputContainer>
+        </InputContainer>
       </HotelPickerContainer>
     </div>
   );
