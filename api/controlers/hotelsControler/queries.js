a576f04e927c3d4c368a69ae3f8d95b05d139ae7
yetabalij <yetabalij@gmail.com>
2022-05-18 14:58:03 +0300

    a basic serch queriy.

--- api/controlers/hotelsControler/queries.js
+++ api/controlers/hotelsControler/queries.js
@@ -3,7 +3,13 @@ const Hotel = require("./../../models/hotels");
 const Room = require("./../../models/rooms");
 exports.searchHotels = async (req, res, next) => {
   try {
-    const result = await Hotel.find({ city: req.body.city }).populate("rooms");
+    const result = await Hotel.find({
+      city: req.body.city,
+      //   age: { $gt: 17, $lt: 66 },
+    }).populate({
+      path: "rooms",
+      match: { maxPeople: { $gte: req.body.maxPeople } },
+    });
     res.send(result);
   } catch (error) {}
 };
