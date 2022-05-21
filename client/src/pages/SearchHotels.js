c7754c30fb37d76fa8b270c5ae856ea1f233fd8d
yetabalij <ermiastsegu@yahoo.com>
2022-05-21 20:56:28 +0300

    logic for conditional rendering.

--- client/src/pages/SearchHotels.js
+++ client/src/pages/SearchHotels.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
-//import { searchHotels } from "./../redux/features/searchSlice";
+import NavBar from "./../components/NavBar";
 
 const SearchHotels = () => {
   const { Hotels } = useSelector((state) => ({ ...state.searchHotels }));
-  const res = Hotels.map((room) => {
-    if (room.hotel.city === "city 3" ) {
-      return console.log(room);
+  const res = Hotels.map((room, key) => {
+    if (
+      room.hotel !== null &&
+      room.checkIn === null &&
+      room.checkOut === null
+    ) {
+      return <h3 key={room._id}>{room.title}</h3>;
+    } else {
+      return <h2>List not Available</h2>;
     }
   });
-  //console.log(Hotels);
-  return <div>SearchHotels</div>;
+  return (
+    <NavBar>
+      <p>Hello Ethiopia</p>
+      <div>{res}</div>
+    </NavBar>
+  );
 };
 
 export default SearchHotels;
