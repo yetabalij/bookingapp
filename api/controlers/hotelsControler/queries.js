4b8e6630815211eab7d88248c73752ac2c398bb6
yetabalij <ermiastsegu@yahoo.com>
2022-05-21 19:03:53 +0300

    hotel search queriy is modified.

--- api/controlers/hotelsControler/queries.js
+++ api/controlers/hotelsControler/queries.js
@@ -1,12 +1,17 @@
 //const { createError } = require("./../../utils/error");
 const Hotel = require("./../../models/hotels");
+const Room = require("./../../models/rooms");
 
 exports.searchHotels = async (req, res, next) => {
   try {
-    const result = await Hotel.find({
-      city: req.body.city,
-    }).populate("rooms");
-    path: "rooms", res.send(result);
+    const result = await Room.find({
+      maxPeople: req.body.maxPeople,
+    })
+      .populate({
+        path: "hotel",
+        match: { city: { $eq: req.body.city } },
+      });
+    res.send(result);
   } catch (err) {
     next(err);
   }
