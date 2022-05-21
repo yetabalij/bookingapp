c9cc8fdf644c2cc48868f9a47d3f5f4e8739d728
yetabalij <ermiastsegu@yahoo.com>
2022-05-21 21:07:27 +0300

    available property information.

--- client/src/pages/SearchHotels.js
+++ client/src/pages/SearchHotels.js
@@ -4,22 +4,27 @@ import NavBar from "./../components/NavBar";
 
 const SearchHotels = () => {
   const { Hotels } = useSelector((state) => ({ ...state.searchHotels }));
+
   const res = Hotels.map((room, key) => {
     if (
       room.hotel !== null &&
       room.checkIn === null &&
       room.checkOut === null
     ) {
-      return <h3 key={room._id}>{room.title}</h3>;
+      return (
+        <div key={room._id}>
+          <p>{`${Hotels.length} Property Available`}</p>
+          <h3>{room.title}</h3>
+        </div>
+      );
     } else {
       return <h2>List not Available</h2>;
     }
   });
   return (
-    <NavBar>
-      <p>Hello Ethiopia</p>
+    <div>
       <div>{res}</div>
-    </NavBar>
+    </div>
   );
 };
 
