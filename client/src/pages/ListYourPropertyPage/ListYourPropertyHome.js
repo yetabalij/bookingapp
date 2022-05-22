107b08fb090eb105e39bb8732aefebfcaaa87181
yetabalij <ermiastsegu@yahoo.com>
2022-05-22 20:16:17 +0300

    basic structure for body and footer.

--- client/src/pages/ListYourPropertyPage/ListYourPropertyHome.js
+++ client/src/pages/ListYourPropertyPage/ListYourPropertyHome.js
@@ -1,12 +1,36 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const AppContainer = styled.div`
+  ${tw`
+    
+  `}
+`;
+
+const BodyContainer = styled.div`
+  ${tw`
+    overflow-hidden
+    md:w-4/5
+    2xl:w-3/5
+    mx-auto
+    `}
+`;
+
+const FooterContainer = styled.div`
+  ${tw`
+    bg-primary-color
+    `}
+`;
 
 const ListYourPropertyHome = () => {
   return (
-    <div>
+    <AppContainer>
       <NavBar></NavBar>
-      ListYourPropertyHome
-    </div>
+      <BodyContainer>Hello</BodyContainer>
+      <FooterContainer>Footer</FooterContainer>
+    </AppContainer>
   );
 };
 
