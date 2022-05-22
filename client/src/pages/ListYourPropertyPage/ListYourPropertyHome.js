bc2f4aa7017f9f65248dd0165bbcbd6197c1978a
yetabalij <ermiastsegu@yahoo.com>
2022-05-22 22:19:09 +0300

    basic sections are started.

--- client/src/pages/ListYourPropertyPage/ListYourPropertyHome.js
+++ client/src/pages/ListYourPropertyPage/ListYourPropertyHome.js
@@ -1,36 +1,24 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
-import styled from "styled-components";
-import tw from "twin.macro";
-
-const AppContainer = styled.div`
-  ${tw`
-    
-  `}
-`;
-
-const BodyContainer = styled.div`
-  ${tw`
-    overflow-hidden
-    md:w-4/5
-    2xl:w-3/5
-    mx-auto
-    `}
-`;
-
-const FooterContainer = styled.div`
-  ${tw`
-    bg-primary-color
-    `}
-`;
+import "./ListYourPropertyHome.css";
 
 const ListYourPropertyHome = () => {
   return (
-    <AppContainer>
-      <NavBar></NavBar>
-      <BodyContainer>Hello</BodyContainer>
-      <FooterContainer>Footer</FooterContainer>
-    </AppContainer>
+    <div className="AppContainer">
+      <NavBar />
+      <div className="BodyContainer">
+        <div className="HeaderContainer">
+          <div className="HeaderSection md:w-4/5 2xl:w-3/5 mx-auto flex justify-between">
+            <div className="LeaftSection">
+              <div className="LeftSectionOne">Left section one</div>
+              <div className="LeftSectionTwo">Left section two</div>
+            </div>
+            <div className="RightSection">rightsection</div>
+          </div>
+        </div>
+      </div>
+      <div className="FooterContainer">footer</div>
+    </div>
   );
 };
 
