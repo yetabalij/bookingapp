91b5306ce91d1eae29c70f47b425f8e3792abdfc
yetabalij <ermiastsegu@yahoo.com>
2022-05-25 22:03:46 +0300

    new component added.

--- client/src/pages/ListYourPropertyPage/ListYourPropertyHome.js
+++ client/src/pages/ListYourPropertyPage/ListYourPropertyHome.js
@@ -1,11 +1,13 @@
 import NavBar from "../../components/NavBar";
 import HeaderComp from "./components/HeaderComp";
+import YourPeaceOfMind from "./components/YourPeaceOfMind";
 
 const ListYourPropertyHome = () => {
   return (
     <div className="AppContainer">
       <NavBar />
       <HeaderComp />
+      <YourPeaceOfMind></YourPeaceOfMind>
       <div className="FooterContainer">footer</div>
     </div>
   );
