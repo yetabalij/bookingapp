c9ab474469d1478e0f4543e9643ae6ca235419ef
yetabalij <ermiastsegu@yahoo.com>
2022-05-25 22:07:23 +0300

    container added.

--- client/src/pages/ListYourPropertyPage/components/YourPeaceOfMind.js
+++ client/src/pages/ListYourPropertyPage/components/YourPeaceOfMind.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Container = styled.div`
+  ${tw`
+    w-5/6
+    md:w-4/5
+    2xl:w-3/5
+    mx-auto
+    pt-4
+    md:pt-2
+    lg:pt-3
+    2xl:pt-12
+    justify-items-center
+    `}
+`;
 
 const YourPeaceOfMind = () => {
   return (
-    <div>YourPeaceOfMind</div>
-  )
-}
+    <div>
+      <Container>YourPeaceOfMind</Container>
+    </div>
+  );
+};
 
-export default YourPeaceOfMind
+export default YourPeaceOfMind;
