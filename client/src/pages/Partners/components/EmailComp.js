6fefb7ef57083aeb4a62d645bb41971dd1a326f5
yetabalij <yetabalij@gmail.com>
2022-05-27 16:21:42 +0300

    default structure.

--- client/src/pages/Partners/components/EmailComp.js
+++ client/src/pages/Partners/components/EmailComp.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 const EmailComp = () => {
-  return <div>EmailComp</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-slate-800">
+        Create your partner account
+      </h1>
+      <p className="text-sm text-gray-500 mt-2 mb-8">
+        Create an account to list and manage your property.
+      </p>
+      <form>
+        <label>Email address</label>
+        <br />
+        <input type="email" placeholder="email"></input>
+        <br />
+        <button>Continue</button>
+      </form>
+    </div>
+  );
 };
 
 export default EmailComp;
---
+++ client/src/pages/Partners/components/PasswordComp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PasswordComp = () => {
+  return <div>PasswordComp</div>;
+};
+
+export default PasswordComp;
