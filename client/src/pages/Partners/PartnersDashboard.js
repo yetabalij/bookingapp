12bb4fe4ab57dfbf87f8a793c4e97ecf9b4d1406
yetabalij <yetabalij@gmail.com>
2022-05-27 15:38:30 +0300

    partner register login and dashboard new files are created.

---
+++ client/src/pages/Partners/PartnersDashboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PartnersDashboard = () => {
+  return <div>PartnersDashboard</div>;
+};
+
+export default PartnersDashboard;
---
+++ client/src/pages/Partners/PartnersRegister.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PartnersRegister = () => {
+  return <div>PartnersRegister</div>;
+};
+
+export default PartnersRegister;
