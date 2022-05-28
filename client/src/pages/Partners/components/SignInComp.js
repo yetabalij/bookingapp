f54f7ed6e4100dad432df6f51a0f1e3d884d9ab7
Ermias <yetabalij@gmail.com>
2022-05-28 21:55:08 +0300

    new component is added and completed.

---
+++ client/src/pages/Partners/components/SignInComp.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  ${tw`
+    
+    `}
+`;
+const HorizontalLine = styled.hr`
+  ${tw`
+    my-5
+`}
+  width: 100%;
+  border: 1px solid #f3f3f3;
+`;
+const PContainer = styled.div`
+  ${tw`
+    
+    `}
+`;
+const Button = styled.button`
+  ${tw`
+    mt-5
+    w-full
+    text-secondary-color
+    bg-white
+    py-2
+    `}
+  border: 1px solid blue;
+`;
+
+const SignInComp = () => {
+  return (
+    <div>
+      <HorizontalLine />
+      <PContainer>
+        <p className="text-xs">
+          Do you have questions about your property or the extranet? Visit the
+        </p>
+        <p className="text-xs">
+          <span className="text-blue-500">
+            <Link to="/">Partner Help Center</Link>
+          </span>
+          or ask another partner on the{" "}
+          <span className="text-blue-500">
+            <Link to="/">Partner Community</Link>
+          </span>
+          .
+        </p>
+      </PContainer>
+      <Button>Sign in</Button>
+      <HorizontalLine />
+      <PContainer>
+        <p className="text-xs">
+          By signing in or creating an account, you agree with our
+        </p>
+        <p className="text-xs">
+          <span className="text-blue-500">
+            <Link to="/">Terms and Conditions</Link>
+          </span>
+          and
+          <span className="text-blue-500">
+            <Link to="/">Privacy Statement</Link>
+          </span>
+          .
+        </p>
+      </PContainer>
+      <HorizontalLine />
+      <p className="text-xs">All rights reserved.</p>
+    </div>
+  );
+};
+
+export default SignInComp;
