3f04f63b5a3eee2f8f09f297fd3be5eb5c725661
Ermias <yetabalij@gmail.com>
2022-05-29 14:43:56 +0300

    partnersAuthReducer is added to configureStore.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./features/searchSlice";
+import partnersAuthReducer from "./features/Partners/partnersAuthSlice";
 
 export const store = configureStore({
-  reducer: { searchHotels: searchReducer },
+  reducer: { searchHotels: searchReducer, partnersAuth: partnersAuthReducer },
 });
