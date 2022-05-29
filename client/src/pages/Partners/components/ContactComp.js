a417ddbd41a2c2f7405d41a7ed2c7f4887ac4d41
Ermias <yetabalij@gmail.com>
2022-05-29 14:24:07 +0300

    component is completed.

--- client/src/pages/Partners/components/ContactComp.js
+++ client/src/pages/Partners/components/ContactComp.js
@@ -1,7 +1,49 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
-const ContactComp = () => {
-  return <div>ContactComp</div>;
+const FormContainer = styled.div`
+  ${tw`
+  
+  `}
+`;
+const Input = styled.input`
+  ${tw`
+  w-full
+  p-1
+  mb-3
+  `}
+  border: 1px solid gray;
+`;
+
+const ContactComp = ({ firstName, lastName, setFirstName, setLastName }) => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-slate-800">Contact details</h1>
+      <p className="text-sm text-gray-500 mt-2 mb-8">
+        Create an account to list and manage your property.
+      </p>
+      <FormContainer>
+        <label>First Name</label>
+        <br />
+        <Input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="First Name"
+        ></Input>
+        <br />
+        <label>Last Name</label>
+        <br />
+        <Input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          placeholder="Last Name"
+        ></Input>
+      </FormContainer>
+    </div>
+  );
 };
 
 export default ContactComp;
