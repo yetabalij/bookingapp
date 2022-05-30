482ede075a6cba92d0f29a7e76c0c1bc1de10168
yetabalij <ermiastsegu@yahoo.com>
2022-05-30 22:33:36 +0300

    logout functionality is completed.

--- client/src/pages/Partners/PartnersDashboard.js
+++ client/src/pages/Partners/PartnersDashboard.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPartnerLogout } from "../../redux/features/Partners/partnersAuthSlice";
+import { useNavigate } from "react-router-dom";
 
 const PartnersDashboard = () => {
-  return <div>PartnersDashboard</div>;
+  const dispach = useDispatch();
+  const navigate = useNavigate()
+
+  const logoutHandler = () => {
+    dispach(setPartnerLogout());
+    navigate("/partnersignin");
+  };
+  return (
+    <div>
+      PartnersDashboard
+      <br />
+      <button onClick={logoutHandler}>logout</button>
+    </div>
+  );
 };
 
 export default PartnersDashboard;
