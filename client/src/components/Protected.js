3512c3dffcf847e40d36f43d0893b6aa08267173
yetabalij <yetabalij@gmail.com>
2022-05-30 15:30:37 +0300

    modifed to use partners

--- client/src/components/Protected.js
+++ client/src/components/Protected.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useState } from "react";
+
 import { Outlet, Navigate } from "react-router-dom";
 
-const Protected = () => {
-  const [token] = useState(false);
-  return token ? <Outlet /> : <Navigate to="/partnersignin" />;
+const Protected = ({ partner }) => {
+  return partner ? <Outlet /> : <Navigate to="/partnersignin" />;
 };
 
 export default Protected;
