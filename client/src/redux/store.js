e38b02fb596682d09d2882e2f4f5206aec3c4419
yetabalij <yetabalij@gmail.com>
2022-06-01 17:08:34 +0300

    property reducer added.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./features/searchSlice";
 import partnersAuthReducer from "./features/Partners/partnersAuthSlice";
+import propertyReducer from "./features/Properties/propertySlice";
 
 export const store = configureStore({
-  reducer: { searchHotels: searchReducer, partnersAuth: partnersAuthReducer },
+  reducer: {
+    searchHotels: searchReducer,
+    partnersAuth: partnersAuthReducer,
+    properties: propertyReducer,
+  },
 });
