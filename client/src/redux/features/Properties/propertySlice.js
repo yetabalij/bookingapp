7e7a9b5b057cfb43b39f91262fff8f621aece25b
yetabalij <yetabalij@gmail.com>
2022-06-01 17:18:12 +0300

    property slice and create action implemented.

---
+++ client/src/redux/features/Properties/propertySlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const createHotel = createAsyncThunk(
+  "create/hotel",
+  async ({ formValue, navigate }) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/hotels",
+        formValue
+      );
+      navigate("/partnersdashboard");
+      return response.data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+const propertySlice = createSlice({
+  name: "property",
+  initialState: {
+    Property: null,
+    error: "",
+    loading: "",
+  },
+  reducers: {
+    [createHotel.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [createHotel.fulfilled]: (state, action) => {
+      state.loading = false;
+      //localStorage.setItem("property", JSON.stringify({ ...action.payload }));
+      state.Partners = action.payload;
+    },
+    [createHotel.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload.message;
+    },
+  },
+  extraReducers: {},
+});
+
+export default propertySlice.reducer;
