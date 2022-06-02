51fea6dc34478d164384c8011685d177e4370625
yetabalij <yetabalij@gmail.com>
2022-06-02 15:22:54 +0300

    cloudinary file is created.

---
+++ api/controlers/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dpy3euo56",
+  api_key: "485554254842889",
+  api_secret: "Hxa7JkR3L5TimiBIrZ2NzaQRKpU",
+});
+
+module.exports = cloudinary;
