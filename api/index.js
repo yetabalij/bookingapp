426b8a0dc97872230a943f7d216796cb979a2af1
yetabalij <yetabalij@gmail.com>
2022-06-02 15:41:01 +0300

    cloudniary added and middlewares are modifed.

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const mogran = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const cloudinary = require("./controlers/cloudinary");
 
 //imported routeres
 const authRoute = require("../api/routes/auth");
@@ -20,7 +21,8 @@ dotenv.config();
 const app = express();
 
 //middlwares
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors());
 app.use(mogran("dev"));
