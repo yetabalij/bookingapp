4da43e000a557196f95a83aba365978ecf833a5b
Ermias <yetabalij@gmail.com>
2022-06-05 15:56:05 +0300

    property type route added.

--- api/routes/hotels/queries.js
+++ api/routes/hotels/queries.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { searchHotels } = require("../../controlers/hotelsControler/queries");
+const {
+  searchHotels,
+  propertyType,
+} = require("../../controlers/hotelsControler/queries");
 
 const router = express.Router();
 
 router.post("/search", searchHotels);
+router.get("/propertyType", propertyType);
 
 module.exports = router;
