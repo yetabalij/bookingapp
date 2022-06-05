6ed5a3046e7cfeb63d0ad463fec3b7e1b14cce27
Ermias <yetabalij@gmail.com>
2022-06-05 16:01:15 +0300

    property type component added.

--- client/src/pages/HomePage/Home.js
+++ client/src/pages/HomePage/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../../components/NavBar";
 import MainHeader from "../../components/MainHeader";
 import HotelPicker from "./components/HotelPicker";
+import PropertyType from "./components/PropertyType";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <NavBar />
       <MainHeader />
       <HotelPicker />
+      <PropertyType />
       Home
     </div>
   );
