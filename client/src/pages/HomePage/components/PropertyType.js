b3b677869d7659f871e78c5a9ac4d305c5e24138
Ermias <yetabalij@gmail.com>
2022-06-05 19:25:14 +0300

    Browse by property type is created.

--- client/src/pages/HomePage/components/PropertyType.js
+++ client/src/pages/HomePage/components/PropertyType.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const PropertyType = () => {
-  return <div>PropertyType</div>;
+  return <div>Browse by property type</div>;
 };
 
 export default PropertyType;
