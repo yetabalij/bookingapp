5cd8562722238bb0369b180e8b1417c9e2a120ac
yetabalij <ermiastsegu@yahoo.com>
2022-06-06 11:15:08 +0300

    new directory.

---
+++ client/src/pages/HelpPage/Help.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+
+const Help = () => {
+  return (
+    <div>
+      <NavBar></NavBar>help
+    </div>
+  );
+};
+
+export default Help;
