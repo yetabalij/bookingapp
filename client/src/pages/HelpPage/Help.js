ad14a01f8c1b99b4e9f3898b0f91b257930ed51e
yetabalij <ermiastsegu@yahoo.com>
2022-06-06 11:33:34 +0300

    help is started.

--- client/src/pages/HelpPage/Help.js
+++ client/src/pages/HelpPage/Help.js
@@ -1,10 +1,10 @@
 import React from "react";
-import NavBar from "../../components/NavBar";
+import SecondaryNavBar from "./../../components/SecondaryNavBar";
 
 const Help = () => {
   return (
     <div>
-      <NavBar></NavBar>help
+      <SecondaryNavBar></SecondaryNavBar>
     </div>
   );
 };
