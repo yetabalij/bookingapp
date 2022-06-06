8faae5a11856ec3f93d34520c292dcc07245af4a
yetabalij <ermiastsegu@yahoo.com>
2022-06-06 13:55:55 +0300

    component added.

--- client/src/pages/HelpPage/Help.js
+++ client/src/pages/HelpPage/Help.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import HelpContacts from "./Components/HelpContacts";
 import SecondaryNavBar from "./../../components/SecondaryNavBar";
+import FrequentlyAsked from "./Components/FrequentlyAsked";
 
 const Help = () => {
   return (
     <div>
       <SecondaryNavBar />
       <HelpContacts />
+      <FrequentlyAsked />
     </div>
   );
 };
