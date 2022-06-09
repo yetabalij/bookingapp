db67f5d059fc71f025083bfe521878838af08b51
yetabalij <yetabalij@gmail.com>
2022-06-09 11:42:24 +0300

    route added.

--- api/routes/hotels/queries.js
+++ api/routes/hotels/queries.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   searchHotels,
   propertyType,
+  propertyByCity,
 } = require("../../controlers/hotelsControler/queries");
 
 const router = express.Router();
 
 router.post("/search", searchHotels);
 router.get("/propertyType", propertyType);
+router.get("/propertybycity", propertyByCity);
 
 module.exports = router;
