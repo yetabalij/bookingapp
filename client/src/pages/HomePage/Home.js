0420db0b9be385b6d4c8c0974fae08b394ac3232
yetabalij <yetabalij@gmail.com>
2022-06-09 11:27:06 +0300

    new file is added.

--- client/src/pages/HomePage/Home.js
+++ client/src/pages/HomePage/Home.js
@@ -3,6 +3,7 @@ import NavBar from "../../components/NavBar";
 import MainHeader from "../../components/MainHeader";
 import HotelPicker from "./components/HotelPicker";
 import PropertyType from "./components/PropertyType";
+import CityProperty from "./components/CityProperty";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <MainHeader />
       <HotelPicker />
       <PropertyType />
+      <CityProperty />
       Home
     </div>
   );
---
+++ client/src/pages/HomePage/components/CityProperty.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CityProperty = () => {
+  return <div>CityProperty</div>;
+};
+
+export default CityProperty;
