5cf7d97197fef74d774d6aeb9cda8b7e46ddd1f4
yetabalij <ermiastsegu@yahoo.com>
2022-06-10 21:38:45 +0300

    cors middleware modified.

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(mogran("dev"));
 
 //router middlewares
