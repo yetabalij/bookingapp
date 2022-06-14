5583c01e2e9d466ff7bbaba4a53c9537710659c3
yetabalij <yetabalij@gmail.com>
2022-06-14 11:13:43 +0300

    modified.

--- client/src/pages/Partners/PartnersDashboard.js
+++ client/src/pages/Partners/PartnersDashboard.js
@@ -1,38 +1,17 @@
 import React, { useState, useEffect } from "react";
+
 import axios from "axios";
 import styled from "styled-components";
 import tw from "twin.macro";
 
-// import Property from "../Properties/Property";
-// import Rooms from "../Properties/Rooms";
-
 import { useDispatch, useSelector } from "react-redux";
 import { setPartnerLogout } from "../../redux/features/Partners/partnersAuthSlice";
 import { useNavigate } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 const Container = styled.div`
   ${tw`
     
-
-  `}
-`;
-// const LeftContainer = styled.div`
-//   ${tw`
-//   bg-gray-100
-//   `}
-// `;
-// const RightContainer = styled.div`
-//   ${tw`
-
-//   `}
-// `;
-const Button = styled.button`
-  ${tw`
-    py-2
-    px-6
-    text-white
-    bg-secondary-color
-    font-bold
   `}
 `;
 
@@ -56,17 +35,9 @@ const PartnersDashboard = () => {
       });
   }, []);
 
-  const logoutHandler = () => {
-    dispach(setPartnerLogout());
-    navigate("/partnersignin");
-  };
-
   return (
     <Container>
-      <h1>dash board</h1>
-      <h3>wellcome {username}</h3>
-      {partnerProperty !== null && <h3>Property, {partnerProperty[0].name}</h3>}
-      <Button onClick={logoutHandler}>Sign Out</Button>
+      <NavBar />
     </Container>
   );
 };
