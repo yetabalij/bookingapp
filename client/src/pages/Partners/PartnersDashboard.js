33533a1a0af1ca2b36e9d8b6c68c2bd91d1d2352
yetabalij <yetabalij@gmail.com>
2022-06-14 12:16:28 +0300

    partners dashboard cleande.

--- client/src/pages/Partners/PartnersDashboard.js
+++ client/src/pages/Partners/PartnersDashboard.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import axios from "axios";
 import styled from "styled-components";
 import tw from "twin.macro";
 
-import { useDispatch, useSelector } from "react-redux";
-import { setPartnerLogout } from "../../redux/features/Partners/partnersAuthSlice";
-import { useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 const Container = styled.div`
@@ -16,24 +12,8 @@ const Container = styled.div`
 `;
 
 const PartnersDashboard = () => {
-  const partner = JSON.parse(localStorage.getItem("partnerProfile"));
-  const { username, _id } = partner;
-
-  const formValue = {
-    partnerId: _id,
-  };
-
-  const dispach = useDispatch();
-  const navigate = useNavigate();
-
-  const [partnerProperty, setPartnerProperty] = useState(null);
-  useEffect(() => {
-    axios
-      .post("http://localhost:8000/api/hotels/propertybypartner", formValue)
-      .then((res) => {
-        setPartnerProperty(res.data);
-      });
-  }, []);
+  const partnerProperty = JSON.parse(localStorage.getItem("partnerProperty"));
+  console.log(partnerProperty);
 
   return (
     <Container>
