03f632e1e9012c49e8f3b06184d5ef3ad5be934f
yetabalij <yetabalij@gmail.com>
2022-06-14 16:29:54 +0300

    token changed.

--- api/utils/verityToken.js
+++ api/utils/verityToken.js
@@ -2,9 +2,8 @@ const jwt = require("jsonwebtoken");
 const { createError } = require("./error");
 
 exports.verifyToken = (req, res, next) => {
-  console.log(req.headers);
-  const token = req.headers["authorization"];
-  //console.log(token);
+  //console.log(req.headers.cookie.split("=")[1]);
+  const token = req.headers.cookie.split("=")[1];
   if (!token) {
     return next(createError(401, "You are not authenticated!"));
   }
