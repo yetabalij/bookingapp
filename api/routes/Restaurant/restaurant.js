7250239d04b3a1d9b75403bcd35e3919a0e8e516
yetabalij <yetabalij@gmail.com>
2022-06-28 16:04:38 +0300

    getSpa route added.

--- api/routes/Restaurant/restaurant.js
+++ api/routes/Restaurant/restaurant.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getRestaurants,
   getBar,
+  getSpa,
 } = require("./../../controlers/Restaurant/restaurant");
 
 const router = express.Router();
 
 router.get("/restaurant", getRestaurants);
 router.get("/bar", getBar);
+router.get("/spa", getSpa);
 
 module.exports = router;
