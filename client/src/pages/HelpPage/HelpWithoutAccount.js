85bbd0041d46bb250ddb88d152f55edd6449a343
yetabalij <yetabalij@gmail.com>
2022-06-29 11:42:49 +0300

    help without contact form is completed.

--- client/src/pages/HelpPage/HelpWithoutAccount.js
+++ client/src/pages/HelpPage/HelpWithoutAccount.js
@@ -1,10 +1,77 @@
 import React from "react";
+import NavBar from "./../../components/SecondaryNavBar";
 import Footer from "../../components/Footer";
 
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Container = styled.div`
+  ${tw`
+   w-2/5
+   mx-auto
+   mt-20
+  `}
+`;
+const Card = styled.div`
+  ${tw`
+        bg-white
+        p-8
+        mt-4
+        flex
+        flex-col
+      justify-between
+      justify-items-center
+`}
+  box-shadow: rgba(40, 40, 93, 0.15) 0px 1px 2px 1px, rgba(0, 0, 0, 0.3) 0px 1px 1px -1px;
+`;
+const Input = styled.input`
+  ${tw`
+  w-full
+  p-1
+  mb-3
+  `}
+  border: 1px solid gray;
+`;
+const Button = styled.button`
+  ${tw`
+    bg-secondary-color
+    text-white
+    py-3
+    px-4
+    text-sm
+    font-bold
+    mt-6
+    `}
+`;
+
 const HelpWithoutAccount = () => {
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    alert("information submited.");
+  };
   return (
     <div>
-      HelpWithoutAccount <Footer />
+      <NavBar />
+      <Container>
+        <Card>
+          <p>
+            provide us your contact details and we will reach you immediately
+          </p>
+          <form onSubmit={handelSubmit} className="mt-5">
+            <label>Full Name</label>
+            <br />
+            <Input className="" type="text" />
+            <label>Email</label>
+            <br />
+            <Input className="" type="email" />
+            <label>Phone</label>
+            <br />
+            <Input className="" type="text" />
+            <Button type="submit">Contact Us</Button>
+          </form>
+        </Card>
+      </Container>
+      <Footer />
     </div>
   );
 };
