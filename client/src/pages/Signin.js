39ff3fef9a7fe33e5ac1ea1a43ca50740ba71908
yetabalij <yetabalij@gmail.com>
2022-06-29 11:03:38 +0300

    modified.

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/ThirdNavBar";
 import Footer from "../components/Footer";
 
 const Signin = () => {
