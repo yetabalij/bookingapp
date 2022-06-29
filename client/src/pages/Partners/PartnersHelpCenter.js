51dfe4eff945a2cd758c3bffe3636ea827181ad4
yetabalij <yetabalij@gmail.com>
2022-06-29 11:52:05 +0300

    header added.

--- client/src/pages/Partners/PartnersHelpCenter.js
+++ client/src/pages/Partners/PartnersHelpCenter.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NavBar from "./../../components/SecondaryNavBar";
 import Footer from "../../components/Footer";
 
 const PartnersHelpCenter = () => {
   return (
     <div>
+      <NavBar />
       PartnersHelpCenter <Footer />
     </div>
   );
