9fd3b68e62c4320ec4b8fb1730d0a5005edb58eb
yetabalij <yetabalij@gmail.com>
2022-07-01 15:42:32 +0300

    unused conde is removed and card component is added.

--- client/src/pages/Partners/Room.js
+++ client/src/pages/Partners/Room.js
@@ -3,9 +3,10 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 import tw from "twin.macro";
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
+import Card from "./../../components/Card";
 import Footer from "../../components/Footer";
 
 const Container = styled.div`
@@ -23,18 +24,7 @@ const ContentContainer = styled.div`
     justify-items-center
   `}
 `;
-const Card = styled.div`
-  ${tw`
-        bg-white
-        p-8
-        mt-4
-        flex
-        flex-col
-      justify-between
-      justify-items-center
-`}
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 1px 2px 1px, rgba(0, 0, 0, 0.3) 0px 1px 1px -1px;
-`;
+
 const Button = styled.button`
   ${tw`
     bg-secondary-color
@@ -49,12 +39,10 @@ const Button = styled.button`
 
 const Room = () => {
   const { Property } = useSelector((state) => ({ ...state.properties }));
-  const [propertyId, setPropertyId] = useState("");
   const [data, setData] = useState([]);
 
   useEffect(() => {
     if (Property !== null && Property[0]?._id) {
-      setPropertyId(Property[0]?._id);
       axios
         .get(
           `http://localhost:8000/api/rooms/roomsbyproperty/${Property[0]?._id}`
