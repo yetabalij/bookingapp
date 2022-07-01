d0f3f4e157a70eadf9825ab173f700efa2ac3054
yetabalij <yetabalij@gmail.com>
2022-07-01 15:38:04 +0300

    unused code is commented and layout card component is added.

--- client/src/pages/Partners/Reservation.js
+++ client/src/pages/Partners/Reservation.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import tw from "twin.macro";
 import NavBar from "./components/NavBar";
 import Footer from "../../components/Footer";
+import Card from "./../../components/Card";
 
 const Container = styled.div`
   ${tw`
@@ -19,29 +20,18 @@ const ContentContainer = styled.div`
     justify-items-center
   `}
 `;
-const Card = styled.div`
-  ${tw`
-        bg-white
-        p-8
-        mt-4
-        flex
-        flex-col
-      justify-between
-      justify-items-center
-`}
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 1px 2px 1px, rgba(0, 0, 0, 0.3) 0px 1px 1px -1px;
-`;
-const Button = styled.button`
-  ${tw`
-    bg-secondary-color
-    text-white
-    py-3
-    px-4
-    text-sm
-    font-bold
-    mt-6
-    `}
-`;
+
+// const Button = styled.button`
+//   ${tw`
+//     bg-secondary-color
+//     text-white
+//     py-3
+//     px-4
+//     text-sm
+//     font-bold
+//     mt-6
+//     `}
+// `;
 
 const Reservation = () => {
   return (
