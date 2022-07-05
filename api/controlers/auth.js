e020821642e55077fc393c123672fa5499b53113
yetabalij <yetabalij@gmail.com>
2022-07-05 10:35:02 +0300

    modified.

--- api/controlers/auth.js
+++ api/controlers/auth.js
@@ -1,6 +1,5 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
 const User = require("./../models/users");
 const { createError } = require("./../utils/error");
 
@@ -12,9 +11,19 @@ exports.register = async (req, res, next) => {
       username: req.body.username,
       email: req.body.email,
       password: hash,
+      role: "gust",
     });
     await newUser.save();
-    res.status(200).json("User has been created.");
+    const token = jwt.sign(
+      { id: newUser._id, name: newUser.username, role: newUser.role },
+      process.env.JWT_SEKRETE,
+      { expiresIn: "1h" }
+    );
+    const { password, role, ...otherDetails } = newUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(otherDetails);
   } catch (err) {
     next(err);
   }
@@ -36,14 +45,15 @@ exports.login = async (req, res, next) => {
       return next(createError(400, "wrong password or username"));
 
     const token = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.JWT_SEKRETE
+      { id: newUser._id, name: newUser.username, role: newUser.role },
+      process.env.JWT_SEKRETE,
+      { expiresIn: "1h" }
     );
-    const { password, isAdmin, ...otherDetails } = user._doc;
+    const { password, role, ...otherDetails } = newUser._doc;
     res
-      // .cookie("access_token", token, { httpOnly: true })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json({ otherDetails, token });
+      .json(otherDetails);
   } catch (err) {
     next(err);
   }
