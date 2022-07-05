f6ce06a584fa0be65bffa7aaacd2f657bed2cddf
yetabalij <yetabalij@gmail.com>
2022-07-05 10:49:27 +0300

    directory added.

---
+++ client/src/pages/Gust/Register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ThirdNavBar from "../../components/ThirdNavBar";
+import Footer from "../../components/Footer";
+const Register = () => {
+  return (
+    <div>
+      <ThirdNavBar />
+      Register
+      <Footer />
+    </div>
+  );
+};
+
+export default Register;
---
+++ client/src/pages/Gust/Signin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../../components/ThirdNavBar";
+import Footer from "../../components/Footer";
+
+const Signin = () => {
+  return (
+    <div>
+      <NavBar></NavBar>Signin ok
+      <Footer />
+    </div>
+  );
+};
+
+export default Signin;
