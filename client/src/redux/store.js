2838a868f05955a088a0302d2f66367fb5af3016
yetabalij <yetabalij@gmail.com>
2022-07-05 14:55:33 +0300

    reduce modified.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -9,6 +9,6 @@ export const store = configureStore({
     searchHotels: searchReducer,
     partnersAuth: partnersAuthReducer,
     properties: propertyReducer,
-    gustReducer,
+    gust: gustReducer,
   },
 });
