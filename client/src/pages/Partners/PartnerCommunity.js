3ee93166de0c2e45a263d5bafbca93f7796529c2
yetabalij <yetabalij@gmail.com>
2022-07-05 12:03:33 +0300

    navigation is added.

--- client/src/pages/Partners/PartnerCommunity.js
+++ client/src/pages/Partners/PartnerCommunity.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SecondaryNavBar from "./../../components/SecondaryNavBar";
 import Footer from "../../components/Footer";
 
 const PartnerCommunity = () => {
   return (
     <div>
+      <SecondaryNavBar />
       PartnerCommunity <Footer />
     </div>
   );
