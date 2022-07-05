21997ec009080f22379097c092414223a0409b00
yetabalij <yetabalij@gmail.com>
2022-07-05 12:05:45 +0300

    navigation is added.

--- client/src/pages/PrivacyStatement/PrivacyStatement.js
+++ client/src/pages/PrivacyStatement/PrivacyStatement.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SecondaryNavBar from "./../../components/SecondaryNavBar";
 import Footer from "../../components/Footer";
 
 const PrivacyStatement = () => {
   return (
     <div>
+      <SecondaryNavBar />
       PrivacyStatement <Footer />
     </div>
   );
--- client/src/pages/TermsAndConditions/TermsAndConditions.js
+++ client/src/pages/TermsAndConditions/TermsAndConditions.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SecondaryNavBar from "./../../components/SecondaryNavBar";
 import Footer from "../../components/Footer";
 
 const TermsAndConditions = () => {
   return (
     <div>
+      <SecondaryNavBar />
       TermsAndConditions <Footer />
     </div>
   );
