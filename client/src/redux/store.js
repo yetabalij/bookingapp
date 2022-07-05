34565b90ff525e61d47bd5d7131e984961319c34
yetabalij <yetabalij@gmail.com>
2022-07-05 14:13:29 +0300

    gustReducer is added.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./features/searchSlice";
 import partnersAuthReducer from "./features/Partners/partnersAuthSlice";
 import propertyReducer from "./features/Properties/propertySlice";
+import gustReducer from "./features/Gust/gustAuthSlice";
 
 export const store = configureStore({
   reducer: {
     searchHotels: searchReducer,
     partnersAuth: partnersAuthReducer,
     properties: propertyReducer,
+    gustReducer,
   },
 });
