51c773db09a2fb6181e11ceb548af49697fda0bd
Ermias <yetabalij@gmail.com>
2022-07-09 11:47:31 +0300

    delete room controller is modified.

--- api/controlers/room.js
+++ api/controlers/room.js
@@ -30,16 +30,8 @@ exports.updateRoom = async (req, res, next) => {
 
 //DELETE
 exports.deleteRoom = async (req, res, next) => {
-  const hotelId = req.params.hotelid;
   try {
     await Room.findByIdAndDelete(req.params.id);
-    try {
-      await Hotel.findByIdAndUpdate(hotelId, {
-        $pull: { rooms: req.params.id },
-      });
-    } catch (err) {
-      next(err);
-    }
     res.status(200).json("Room has been deleted");
   } catch (err) {
     next(err);
