0349f5e3b23dc4b1741f3df14b0fdf44b75a36f7
Ermias <yetabalij@gmail.com>
2022-07-11 15:15:30 +0300

    empty field validation implemented.

--- client/src/pages/Partners/components/ContactComp.js
+++ client/src/pages/Partners/components/ContactComp.js
@@ -16,7 +16,14 @@ const Input = styled.input`
   border: 1px solid gray;
 `;
 
-const ContactComp = ({ firstName, lastName, setFirstName, setLastName }) => {
+const ContactComp = ({
+  firstName,
+  lastName,
+  setFirstName,
+  setLastName,
+  clientError,
+  setClientError,
+}) => {
   return (
     <div>
       <h1 className="text-2xl font-bold text-slate-800">Contact details</h1>
@@ -24,18 +31,28 @@ const ContactComp = ({ firstName, lastName, setFirstName, setLastName }) => {
         Create an account to list and manage your property.
       </p>
       <FormContainer>
+        {clientError && (
+          <div className="bg-red-300 py-2 px-3 mb-3">
+            <p>{clientError}</p>
+          </div>
+        )}
         <label>First Name</label>
         <br />
         <Input
+          name="firstName"
           type="text"
           value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+            setClientError(false);
+          }}
           placeholder="First Name"
         ></Input>
         <br />
         <label>Last Name</label>
         <br />
         <Input
+          name="lastName"
           type="text"
           value={lastName}
           onChange={(e) => setLastName(e.target.value)}
