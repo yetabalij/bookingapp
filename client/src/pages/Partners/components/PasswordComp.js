ad37e17d5a5c9268986a7b636adcbc1766452e2d
Ermias <yetabalij@gmail.com>
2022-07-11 15:30:49 +0300

    validation implemented for the password, empty fields and backend error.

--- client/src/pages/Partners/components/PasswordComp.js
+++ client/src/pages/Partners/components/PasswordComp.js
@@ -16,7 +16,12 @@ const Input = styled.input`
   border: 1px solid gray;
 `;
 
-const PasswordComp = ({ password, setPassword }) => {
+const PasswordComp = ({
+  password,
+  setPassword,
+  clientError,
+  setClientError,
+}) => {
   return (
     <div>
       <h1 className="text-2xl font-bold text-slate-800">Create a Password</h1>
@@ -24,12 +29,21 @@ const PasswordComp = ({ password, setPassword }) => {
         Your password must has to be a minimum of 8 characters.
       </p>
       <FormContainer>
+        {clientError && (
+          <div className="bg-red-300 py-2 px-3 mb-3">
+            <p>{clientError}</p>
+          </div>
+        )}
         <label>Password</label>
         <br />
         <Input
+          name="password"
           type="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setClientError(false);
+          }}
           placeholder="password"
         ></Input>
         <br />
