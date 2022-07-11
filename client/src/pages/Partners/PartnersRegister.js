aa92134e0c662396beebef159f2dcc1218b3f48a
Ermias <yetabalij@gmail.com>
2022-07-11 15:32:59 +0300

    validation implemented.

--- client/src/pages/Partners/PartnersRegister.js
+++ client/src/pages/Partners/PartnersRegister.js
@@ -9,8 +9,12 @@ import SignInComp from "./components/SignInComp";
 import Footer from "../../components/Footer";
 
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { partnersRegister } from "../../redux/features/Partners/partnersAuthSlice";
+
+import isEmail from "validator/lib/isEmail";
+import isEmpty from "validator/lib/isEmpty";
+import isStrongPassword from "validator/lib/isStrongPassword";
 
 const Container = styled.div`
   ${tw`
@@ -34,11 +38,14 @@ const Button = styled.button`
 `;
 
 const PartnersRegister = () => {
+  const { error } = useSelector((state) => ({ ...state.partnersAuth }));
   const [CompState, setCompState] = useState(0);
   const [email, setEmail] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
+  const [clientError, setClientError] = useState(false);
+  const [contactError, setContactError] = useState(false);
   const username = `${firstName} ${lastName}`;
 
   const navigate = useNavigate();
@@ -51,17 +58,47 @@ const PartnersRegister = () => {
   };
 
   const incrementCount = () => {
-    setCompState(CompState + 1);
+    if (CompState === 0) {
+      if (isEmpty(email)) {
+        return setClientError("All fields are required.");
+      } else if (!isEmail(email)) {
+        return setClientError("Email must be Valid.");
+      }
+      setCompState((prevState) => prevState + 1);
+    } else if (CompState === 1) {
+      if (isEmpty(firstName) || isEmpty(lastName)) {
+        return setClientError("All fields are required.");
+      }
+      setCompState((prevState) => prevState + 1);
+    } else if (CompState === 2) {
+      setCompState((prevState) => prevState + 1);
+    }
   };
 
   const submitHandler = () => {
-    dispach(partnersRegister({ formValue, navigate }));
-    //console.log(formValue);
+    if (isEmpty(password)) {
+      return setClientError("All fields are required.");
+    } else if (!isStrongPassword(password)) {
+      return setClientError(
+        "Password must contains at least 8 characters with a minimum of 1 upercase, 1 lowercase 1 number and 1 special character"
+      );
+    } else if (error) {
+      setClientError(error);
+    } else {
+      dispach(partnersRegister({ formValue, navigate }));
+    }
   };
 
   const FormRenderer = () => {
     if (CompState === 0) {
-      return <EmailComp email={email} setEmail={setEmail} />;
+      return (
+        <EmailComp
+          email={email}
+          setEmail={setEmail}
+          clientError={clientError}
+          setClientError={setClientError}
+        />
+      );
     } else if (CompState === 1) {
       return (
         <ContactComp
@@ -69,10 +106,19 @@ const PartnersRegister = () => {
           lastName={lastName}
           setFirstName={setFirstName}
           setLastName={setLastName}
+          clientError={clientError}
+          setClientError={setClientError}
         />
       );
     } else {
-      return <PasswordComp password={password} setPassword={setPassword} />;
+      return (
+        <PasswordComp
+          password={password}
+          setPassword={setPassword}
+          clientError={clientError}
+          setClientError={setClientError}
+        />
+      );
     }
   };
 
