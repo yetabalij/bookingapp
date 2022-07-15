cad9ed30ac3bcde10d5c847460e899807e508861
Ermias <yetabalij@gmail.com>
2022-07-15 12:21:27 +0300

    booking logic implemented from searchHotels page.

--- client/src/pages/SearchHotels.js
+++ client/src/pages/SearchHotels.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import NavBar from "./../components/Header";
 import Footer from "../components/Footer";
@@ -8,11 +9,25 @@ import Container from "./../components/Container";
 const SearchHotels = () => {
   const location = useLocation();
   const [data, setData] = useState([]);
+  const gust = JSON.parse(localStorage.getItem("gustProfile"));
+  const navigator = useNavigate();
+  const searchResult = JSON.parse(localStorage.getItem("searchedProperties"));
+
   useEffect(() => {
-    setData(
-      Object.values(JSON.parse(localStorage.getItem("searchedProperties")))
-    );
+    if (searchResult !== null) {
+      setData(Object.values(searchResult));
+    }
   }, [location?.state?.city, location?.state?.maxNumber]);
+
+  const onBookHandler = (d) => {
+    if (gust === null) {
+      localStorage.setItem("savedProperty", JSON.stringify(d));
+      navigator("/signin");
+    } else {
+      localStorage.setItem("savedProperty", JSON.stringify(d));
+      navigator("/booking");
+    }
+  };
 
   return (
     <div>
@@ -28,29 +43,32 @@ const SearchHotels = () => {
               <Card key={d._id}>
                 <div className="grid grid-cols-4 gap-4">
                   <div>
-                    <img src={d.hotel.image} alt="'" />
+                    <img src={d?.hotel?.image} alt="'" />
                   </div>
                   <div>
-                    <p>{d.hotel.name}</p>
-                    <p>{d.hotel.city}</p>
-                    <p>{d.hotel.address}</p>
-                    <p>{d.hotel.distances}</p>
+                    <p>{d?.hotel?.name}</p>
+                    <p>{d?.hotel?.city}</p>
+                    <p>{d?.hotel?.address}</p>
+                    <p>{d?.hotel?.distances}</p>
                   </div>
                   <div>
-                    {d.bar === true ? <p>Bar</p> : null}
-                    {d.breakfast === "Yes" ? <p>Breakfast</p> : null}
-                    {d.freeWifi === true ? <p>Frer WiFi</p> : null}
-                    {d.frontDesk === true ? <p>24/7 Front Desk</p> : null}
-                    {d.hotTube === true ? <p>Sauna Bath</p> : null}
-                    {d.parking === "Yes" ? <p>Parking</p> : null}
-                    {d.spa === true ? <p>Spa</p> : null}
-                    {d.smoking === true ? <p>Smokking Area</p> : null}
+                    {d?.bar === true ? <p>Bar</p> : null}
+                    {d?.breakfast === "Yes" ? <p>Breakfast</p> : null}
+                    {d?.freeWifi === true ? <p>Frer WiFi</p> : null}
+                    {d?.frontDesk === true ? <p>24/7 Front Desk</p> : null}
+                    {d?.hotTube === true ? <p>Sauna Bath</p> : null}
+                    {d?.parking === "Yes" ? <p>Parking</p> : null}
+                    {d?.spa === true ? <p>Spa</p> : null}
+                    {d?.smoking === true ? <p>Smokking Area</p> : null}
                   </div>
                   <div>
                     <p>Price ${d.price}</p>
                     <p>Room Type {d.roomType}</p>
                     <p>Language {d.language}</p>
-                    <button className="bg-primary-color text-white py-1 px-4 mt-2">
+                    <button
+                      className="bg-primary-color text-white py-1 px-4 mt-2"
+                      onClick={() => onBookHandler(d)}
+                    >
                       Book Now
                     </button>
                   </div>
