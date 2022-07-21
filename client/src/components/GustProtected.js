45bb3a4262a194612ca6f0c84fe9361937616edc
Ermias <yetabalij@gmail.com>
2022-07-21 14:59:35 +0300

    bug fixed.

--- client/src/components/GustProtected.js
+++ client/src/components/GustProtected.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import { Outlet, Navigate } from "react-router-dom";
 
-const GustProtected = ({ gust }) => {
+const GustProtected = () => {
+  const gust = JSON.parse(localStorage.getItem("gustProfile"));
   return gust ? <Outlet /> : <Navigate to="/signin" />;
 };
 
