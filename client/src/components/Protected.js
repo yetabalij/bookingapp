f4bea86fef557153198af7b78aa89eec161197a7
Ermias <yetabalij@gmail.com>
2022-07-21 14:59:41 +0300

    bug fixed.

--- client/src/components/Protected.js
+++ client/src/components/Protected.js
@@ -4,7 +4,6 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const Protected = () => {
   const partner = JSON.parse(localStorage.getItem("partnerProfile"));
-  // console.log(partner);
   return partner ? <Outlet /> : <Navigate to="/partnersignin" />;
 };
 
