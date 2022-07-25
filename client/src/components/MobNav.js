188f0c43855b1eb3b370d71ff864bad564342cf4
Ermias <yetabalij@gmail.com>
2022-07-25 13:28:43 +0300

    refactord.

--- client/src/components/MobNav.js
+++ client/src/components/MobNav.js
@@ -22,25 +22,9 @@ const NavBarContainer = styled.div`
     h-full
   `}
 `;
-const Brand = styled.div`
-  ${tw`
-   
-  `}
-`;
-const BrandLeft = styled.div`
-  ${tw`
-    
-  `}
-`;
 const BrandText = styled.h2`
   ${tw`
     text-white
-  `}
-`;
-
-const BrandRight = styled.div`
-  ${tw`
-
   `}
 `;
 const HelpContainer = styled.div`
@@ -62,12 +46,6 @@ const ListYourPropertyContainer = styled.div`
 const Navigation = styled.div`
   ${tw`
     mx-auto
-  `}
-`;
-
-const UlContainer = styled.ul`
-  ${tw`
-    
   `}
 `;
 const LiContainer = styled.li`
@@ -117,119 +95,110 @@ const MobNav = ({ pressed, closeHandler }) => {
               />
             </svg>
           </button>
-
           <div>
             <NavBarContainer>
-              <Brand>
-                <BrandLeft>
-                  <BrandText>
-                    <Link to="/">Real Time</Link>
-                  </BrandText>
-                </BrandLeft>
-                <BrandRight>
-                  <HelpContainer>
-                    <Link to="/help">
-                      <FontAwesomeIcon
-                        style={{ color: "white" }}
-                        icon={faCircleQuestion}
-                        size="xl"
-                      />
-                    </Link>
-                  </HelpContainer>
-                  <ListYourPropertyContainer>
-                    <Link to="/listyourpropertyhome">List Your Property</Link>
-                  </ListYourPropertyContainer>
-                  {gust === null ? (
-                    <Button
-                      className="text-blue-600 bg-white px-4 mr-2 font-medium text-sm"
-                      onClick={() => navigate("/register")}
-                    >
-                      Register
-                    </Button>
-                  ) : (
-                    <>
-                      <div className="p-3 rounded-full bg-secondary-color mr-1">
-                        <FontAwesomeIcon
-                          style={{ color: "white" }}
-                          icon={faUser}
-                          size="xl"
-                        />
-                      </div>
-                      <div className="mt-2 mr-2">
-                        <p className="text-white font-bold">{gust?.username}</p>
-                      </div>
-                    </>
-                  )}
-                  {gust === null ? (
-                    <Button
-                      className="text-blue-600 bg-white px-4 mr-2 font-medium text-sm"
-                      onClick={() => navigate("/signin")}
-                    >
-                      Sign in
-                    </Button>
-                  ) : (
-                    <Button
-                      className="text-blue-600 bg-white px-4 font-medium text-sm"
-                      onClick={handleLogOut}
-                    >
-                      Logout
-                    </Button>
-                  )}
-                </BrandRight>
-              </Brand>
+              <BrandText>
+                <Link to="/">Real Time</Link>
+              </BrandText>
+              <HelpContainer>
+                <Link to="/help">
+                  <FontAwesomeIcon
+                    style={{ color: "white" }}
+                    icon={faCircleQuestion}
+                    size="xl"
+                  />
+                </Link>
+              </HelpContainer>
+              <ListYourPropertyContainer>
+                <Link to="/listyourpropertyhome">List Your Property</Link>
+              </ListYourPropertyContainer>
+              {gust === null ? (
+                <Button
+                  className="text-blue-600 bg-white px-4 mr-2 font-medium text-sm"
+                  onClick={() => navigate("/register")}
+                >
+                  Register
+                </Button>
+              ) : (
+                <>
+                  <div className="p-3 rounded-full bg-secondary-color mr-1">
+                    <FontAwesomeIcon
+                      style={{ color: "white" }}
+                      icon={faUser}
+                      size="xl"
+                    />
+                  </div>
+                  <div className="mt-2 mr-2">
+                    <p className="text-white font-bold">{gust?.username}</p>
+                  </div>
+                </>
+              )}
+              {gust === null ? (
+                <Button
+                  className="text-blue-600 bg-white px-4 mr-2 font-medium text-sm"
+                  onClick={() => navigate("/signin")}
+                >
+                  Sign in
+                </Button>
+              ) : (
+                <Button
+                  className="text-blue-600 bg-white px-4 font-medium text-sm"
+                  onClick={handleLogOut}
+                >
+                  Logout
+                </Button>
+              )}
               <Navigation>
-                <UlContainer>
-                  <LiContainer>
-                    <Link to="/">
-                      <FontAwesomeIcon
-                        style={{ color: "white", marginRight: "10px" }}
-                        icon={faBed}
-                        size="xl"
-                      />
-                      Stay
-                    </Link>
-                  </LiContainer>
-                  <LiContainer>
-                    <Link to="/restaurant">
-                      <FontAwesomeIcon
-                        style={{ color: "white", marginRight: "10px" }}
-                        icon={faUtensils}
-                        size="xl"
-                      />
-                      Restaurant
-                    </Link>
-                  </LiContainer>
-                  <LiContainer>
-                    <Link to="/bar">
-                      <FontAwesomeIcon
-                        style={{ color: "white", marginRight: "10px" }}
-                        icon={faWineGlass}
-                        size="xl"
-                      />
-                      Bar Facilities
-                    </Link>
-                  </LiContainer>
-                  <LiContainer>
-                    <Link to="/Spa">
-                      <FontAwesomeIcon
-                        style={{ color: "white", marginRight: "10px" }}
-                        icon={faSpa}
-                        size="xl"
-                      />
-                      Spa
-                    </Link>
-                  </LiContainer>
-                  <LiContainer>
-                    <Link to="/Sauna">
-                      <FontAwesomeIcon
-                        style={{ color: "white", marginRight: "10px" }}
-                        icon={faBath}
-                        size="xl"
-                      />
-                      Sauna
-                    </Link>
-                  </LiContainer>
-                </UlContainer>
+                <LiContainer>
+                  <Link to="/">
+                    <FontAwesomeIcon
+                      style={{ color: "white", marginRight: "10px" }}
+                      icon={faBed}
+                      size="xl"
+                    />
+                    Stay
+                  </Link>
+                </LiContainer>
+                <LiContainer>
+                  <Link to="/restaurant">
+                    <FontAwesomeIcon
+                      style={{ color: "white", marginRight: "10px" }}
+                      icon={faUtensils}
+                      size="xl"
+                    />
+                    Restaurant
+                  </Link>
+                </LiContainer>
+                <LiContainer>
+                  <Link to="/bar">
+                    <FontAwesomeIcon
+                      style={{ color: "white", marginRight: "10px" }}
+                      icon={faWineGlass}
+                      size="xl"
+                    />
+                    Bar Facilities
+                  </Link>
+                </LiContainer>
+                <LiContainer>
+                  <Link to="/Spa">
+                    <FontAwesomeIcon
+                      style={{ color: "white", marginRight: "10px" }}
+                      icon={faSpa}
+                      size="xl"
+                    />
+                    Spa
+                  </Link>
+                </LiContainer>
+                <LiContainer>
+                  <Link to="/Sauna">
+                    <FontAwesomeIcon
+                      style={{ color: "white", marginRight: "10px" }}
+                      icon={faBath}
+                      size="xl"
+                    />
+                    Sauna
+                  </Link>
+                </LiContainer>
               </Navigation>
             </NavBarContainer>
           </div>
